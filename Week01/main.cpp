d49c122e45fba0cf536ec510605d94b89b9fd52e
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-08-21 09:52:35 +0700

    Exercise 2

--- Week01/main.cpp
+++ Week01/main.cpp
@@ -5,10 +5,18 @@ int main(){
     cout << "======================" << endl;
     cout << "   MY FIRST C++ APP   " << endl;
     cout << "======================" << endl;
+    cout << endl;
     cout << "Name: Truong Tri Hung" << endl;
     cout << "Course: OOP with C++" << endl;
+    cout << "School: HCMUTE" << endl;
     cout << endl;
-    cout << "I'm ready to learn C++!" << endl;
+    cout << "My favorite language C++" << endl;
+    cout << "My goal: Become a software engineer" << endl;
+    cout << "My message: Never stop learning!" << endl;
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "     KEEP CODING      " << endl;
+    cout << "======================" << endl;
 
     return 0;
 }

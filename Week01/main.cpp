ba2cfe6b7bedadfe182c0f5b6d47b192938e4fc6
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-08-21 09:25:07 +0700

    Add formatted output

---
+++ Week01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "======================" << endl;
+    cout << "   MY FIRST C++ APP   " << endl;
+    cout << "======================" << endl;
+    cout << "Name: Truong Tri Hung" << endl;
+    cout << "Course: OOP with C++" << endl;
+    cout << endl;
+    cout << "I'm ready to learn C++!" << endl;
+
+    return 0;
+}
